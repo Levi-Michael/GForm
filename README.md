5621532fcd478b15838a02eaa1af70f34331ae74
unknown <mikilavi@gmail.com>
2023-08-12 19:46:00 +0300

    minor folders, readme

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ Ideas of myself, friend, college etc.
 
 Feel free to fork it and use this skeleton for your specific needs.
 ```
+
+## Thanks
+```
+BIG UP for Google guys that create a lit! repository for gcp modules
+called cloud-foundation-fabric,
+i use it a lot and part of this nodules base on this repo.
+
+```
+> [cloud-foundation-fabric](https://github.com/GoogleCloudPlatform/cloud-foundation-fabric)
---
+++ event-job/maint.tf
@@ -0,0 +1,12 @@
+#TODO 
+## Create a project 
+## Create Service account
+## Folders permissions ?
+## Create Google cloud storage
+## Create Docker artifact registry
+## Create resource local_file Dockerfile
+## Create resource local_file Copy Script
+## Create null resource docker_build Build Image
+## Create Google Cloud run JOB
+## Create Workflow POST run to JOB
+## Create eventarc that trigger the Workflow 

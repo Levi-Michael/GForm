2183c73f265ac2b98ab30fa1a98b0cc79e4ac55e
Levi-Michael <mikilavi@gmail.com>
2023-08-12 20:36:59 +0300

    minor test

--- scheduled-job/maint.tf
+++ scheduled-job/maint.tf
@@ -1,6 +1,21 @@
 #TODO 
+## Locals
+locals {
+  module-source = "../"
+}
 ## Create a project 
-
+module "project" {
+  source          = "${local.module-source}project"
+  billing_account = "123456-123456-123456"
+  name            = "myproject"
+  parent          = "folders/1234567890"
+  prefix          = "foo"
+  services = [
+    "container.googleapis.com",
+    "stackdriver.googleapis.com"
+  ]
+}
+# tftest modules=1 resources=3 inventory=basic.yaml
 ## Create Service account
 ## Folders permissions ?
 ## Create Google cloud storage
